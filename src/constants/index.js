df017f2cdce3767f994cb0331238ba41a5c4536e
Mason Lillipuu <mason.lillipuu@gmail.com>
2025-08-19 14:45:14 -0700

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,267 @@
+export const myProjects = [
+  {
+    id: 1,
+    title: "Portfolio",
+    description:
+      "Facilitates purchases from international websites like Amazon and eBay, allowing customers to shop from these sites and have products delivered domestically.",
+    subDescription: [
+      "Built a scalable application with ASP.NET Core MVC, integrating global platforms like Amazon for domestic delivery.",
+      "Implemented secure authentication and database management using ASP.NET Core Identity and Entity Framework Core.",
+      "Designed a responsive frontend with Tailwind CSS, enhancing user experience.",
+      "Added payment systems, localization, and product filtering for functionality improvements.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/portfolio.PNG",
+    tags: [
+      {
+        id: 1,
+        name: "React.js",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 2,
+        name: "Vite.js",
+        path: "/assets/logos/vitejs.svg",
+      },
+      {
+        id: 3,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Iphone landing page 3D",
+    description:
+      "This is a clone of Apple's iPhone 15 Pro website using React.js and TailwindCSS. It highlights the effective use of GSAP (Greensock Animations) and Three.js for displaying iPhone 15 Pro models in various colors and shapes.",
+    subDescription: [
+      "Enhanced user experience with seamless and captivating animations powered by GSAP.",
+      "Explore the iPhone 15 Pro from every angle with dynamic 3D rendering, offering various color and size options.",
+      "Engage users with a unique and interactive video carousel developed using GSAP for a personalized browsing experience.",
+      "Consistent access and optimal viewing on any device with a fully responsive design that adapts to different screen sizes.",
+    ],
+    href: "https://apple-six-zeta.vercel.app/",
+    logo: "",
+    image: "/assets/projects/apple.PNG",
+    tags: [
+      {
+        id: 1,
+        name: "Three.js",
+        path: "/assets/logos/threejs.svg",
+      },
+      {
+        id: 2,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 3,
+        name: "Gsap",
+        path: "/assets/logos/gsap.svg",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Gaming animated landing page",
+    description:
+      "A visually captivating website inspired by Zentry, featuring scroll-triggered animations, geometric transitions, and engaging video storytelling.",
+    subDescription: [
+      "Interactive 3D transformations that respond to user interactions for a modern feel.",
+      "Seamlessly integrated video elements to enhance storytelling and flow.",
+      "Unique geometric transitions using CSS clip-paths to create visually stunning effects.",
+      "Dynamic animations triggered by scrolling for a more engaging user experience.",
+    ],
+    href: "https://gaming3d.vercel.app/",
+    logo: "",
+    image: "/assets/projects/gaming.PNG",
+    tags: [
+      {
+        id: 1,
+        name: "React.js",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 2,
+        name: "Gsap",
+        path: "/assets/logos/gsap.svg",
+      },
+      {
+        id: 3,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Mojito Landin gpage",
+    description:
+      "GSAP-powered cocktail website using React and Tailwind CSS—a modern, scroll-driven experience packed with advanced animations.",
+    subDescription: [
+      "Power scroll-based animations and timeline control with GSAP’s ScrollTrigger.",
+      "Lock sections in view while animating content for engaging scroll experiences.",
+      "A fully customized carousel with multiple navigation options and animated slides.",
+      "Use scroll-triggered pins and masks for visually striking image transitions.",
+    ],
+    href: "https://cocktails-lemon.vercel.app/",
+    logo: "",
+    image: "/assets/projects/mojito.PNG",
+    tags: [
+      {
+        id: 1,
+        name: "Tailwindcss",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+      {
+        id: 2,
+        name: "Gsap",
+        path: "/assets/logos/gsap.svg",
+      },
+      {
+        id: 3,
+        name: "React.js",
+        path: "/assets/logos/react.svg",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "AI landing page",
+    description:
+      "Modern UI/UX website, developed using React.js and Tailwind CSS, exemplifies modern UI/UX principles. Its sleek design, seamless animations, and overall user experience set a high standard, serving as a reference or inspiration for future modern applications or websites in general.",
+    subDescription: [
+      "Engaging effects triggered by mouse movement and scrolling",
+      "Incorporates modern design elements such as bento grids.",
+      "Enhances visuals with stylish gradients using Tailwind CSS for cards, buttons, etc.",
+      "Ensures seamless functionality and aesthetics across all devices",
+    ],
+    href: "https://ai-landingpage-six.vercel.app/",
+    logo: "",
+    image: "/assets/projects/ai.PNG",
+    tags: [
+      {
+        id: 1,
+        name: "Vite.js",
+        path: "/assets/logos/vitejs.svg",
+      },
+      {
+        id: 2,
+        name: "React.js",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 3,
+        name: "Tailwindcss",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+ 
+];
+
+export const mySocials = [
+ 
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/feed/",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  {
+    name: "Github",
+    href: "https://github.com/masonlillipuu?tab=repositories",
+    icon: "/assets/logos/git.svg",
+  },
+];
+
+export const experiences = [
+  {
+    title: "Web developer & production lead",
+    job: "Euroland IR",
+    date: "2017-2021",
+    contents: [
+      "Developed an HTML5 cross-browser compatible framework for courses to run on mobile and modern browsers with a Flash fallback solution for older",
+      "Communicated with clients to address changes and feedback.",
+      "Extensive use of HTML5, CSS3, XML, and JavaScript including jQuery library",
+      "Built interactions and templates that used JavaScript.",
+    ],
+  },
+  {
+    title: "Front-end developer",
+    job: "Adovinci OÜ",
+    date: "2021-2023",
+    contents: [
+      "Proper user information authentication",
+      "Translate wireframes and designs into good quality code",
+      "Optimize components to work seamlessly across different browsers and devices.",
+      "Enhance application performance with constant monitoring.",
+      "Leverage the inbuilt React toolkit for creating frontend features.",
+    ],
+  },
+  {
+    title: "Freelance Developer",
+    job: "Self-Employed",
+    date: "2023-Present",
+    contents: [
+      "Created a personal portfolio using Three.js, React, Vite, and WebAPI to showcase technical expertise.",
+      "Continuously enhancing technical skills and expanding expertise in modern web development and back-end technologies.",
+    ],
+  },
+];
+export const reviews = [
+  {
+    name: "Jack",
+    username: "@jack",
+    body: "I've never seen anything like this before. It's amazing. I love it.",
+    img: "https://robohash.org/jack",
+  },
+  {
+    name: "Jill",
+    username: "@jill",
+    body: "I don't know what to say. I'm speechless. This is amazing.",
+    img: "https://robohash.org/jill",
+  },
+  {
+    name: "John",
+    username: "@john",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://robohash.org/john",
+  },
+  {
+    name: "Alice",
+    username: "@alice",
+    body: "This is hands down the best thing I've experienced. Highly recommend!",
+    img: "https://robohash.org/alice",
+  },
+  {
+    name: "Bob",
+    username: "@bob",
+    body: "Incredible work! The attention to detail is phenomenal.",
+    img: "https://robohash.org/bob",
+  },
+  {
+    name: "Charlie",
+    username: "@charlie",
+    body: "This exceeded all my expectations. Absolutely stunning!",
+    img: "https://robohash.org/charlie",
+  },
+  {
+    name: "Dave",
+    username: "@dave",
+    body: "Simply breathtaking. The best decision I've made in a while.",
+    img: "https://robohash.org/dave",
+  },
+  {
+    name: "Eve",
+    username: "@eve",
+    body: "So glad I found this. It has changed the game for me.",
+    img: "https://robohash.org/eve",
+  },
+];
